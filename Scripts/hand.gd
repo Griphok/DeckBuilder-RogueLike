2de0a730d91649a01f634703ac33745d092b52d3
Griphok <samuelgyger95@gmail.com>
2024-08-08 14:38:57 +0200

    08.08.24 14:38

--- Scripts/hand.gd
+++ Scripts/hand.gd
@@ -1,7 +1,8 @@
-
+@tool
 extends Node2D
 
 @export var radius: int = 600
+@export var min_max_angle: int = 18
 
 @onready var handsize = $DebugShape
 @onready var card = $DebugShape/Attack_Card
@@ -9,7 +10,9 @@ var starting_position
 
 func calculate_starting_position():
 	handsize.shape.radius = radius
-	starting_position = handsize.global_position + radius
+	starting_position = handsize.global_position + Vector2(0, -radius)
+	card.global_position = starting_position
+	
 func _process(delta):
 	calculate_starting_position()
-	card.global_positionq = starting_position
+	
